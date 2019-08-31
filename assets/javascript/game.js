72dfc320c472b3a20740b46b4db0c93c139b1ba3
Randamus <rcviets@gmail.com>
2019-08-30 21:24:04 -0600

    game reset js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,3 +21,25 @@ var gameStarted = false;
 var gameFinish = false;
 var wins = 0;
 var losses = 0;
+
+function resetGame() {
+    remainingGuesses = limit;
+    gameStarted = false;
+
+currentWordIndex = Math.floor(Math.random() * (words.length));
+
+lettersGuessed = [];
+secretWord = [];
+
+for (var i = 0; i < words[currentWordIndex].length; i++) {
+    secretWord.push("_");
+}
+document.getElementById(play-again).style.cssText = "display: none";
+document.getElementById(hint).style.cssText = "display: none";
+document.getElementById(you-win).style.cssText = "display: none";
+document.getElementById(you-lose).style.cssText = "display: none";
+
+updateDisplay();
+
+};
+
