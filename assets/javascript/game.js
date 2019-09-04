fa9c803bff2a3f9d0cd3ce8e4fc042d700ef82e6
Randamus <rcviets@gmail.com>
2019-09-03 20:49:15 -0600

    all winning images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -40,6 +40,7 @@ function startGame() {
     // Update HTML
     document.getElementById("currentWord").innerHTML = " " + spotsRight.join(" ");
 
+
     console.log(secretWord)
     console.log(secretLetters)
     console.log(spots)
@@ -50,7 +51,31 @@ function startGame() {
 function img () {
     if (secretWord === words[0]) {
         document.getElementById("image").src="assets/images/decepticon.jpg";
+    }
+    else if (secretWord === words[1]) {
+        document.getElementById("image").src="assets/images/cybertron.jpg";
     }    
+    else if (secretWord === words[2]) {
+        document.getElementById("image").src="assets/images/allspark.jpg";
+    }
+    else if (secretWord === words[3]) {
+        document.getElementById("image").src="assets/images/assimilation.jpg";
+    }
+    else if (secretWord === words[4]) {
+        document.getElementById("image").src="assets/images/megatron.jpg";
+    }
+    else if (secretWord === words[5]) {
+        document.getElementById("image").src="assets/images/starscream.jpg";
+    }
+    else if (secretWord === words[6]) {
+        document.getElementById("image").src="assets/images/soundwave.jpg";
+    }
+    else if (secretWord === words[7]) {
+        document.getElementById("image").src="assets/images/barricade.jpg";
+    }
+    else if (secretWord === words[8]) {
+        document.getElementById("image").src="assets/images/brawl.jpg";
+    }
     console.log(image)
 }
 
